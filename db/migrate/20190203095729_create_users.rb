aecee3d27e835ae12bbeb770ae1540ac071482bc
Meet Gujrathi <meetgujrathi@gmail.com>
2019-02-03 17:12:15 +0530

    third commit

--- db/migrate/20190203095729_create_users.rb
+++ db/migrate/20190203095729_create_users.rb
@@ -1,6 +1,11 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
+      t.string :user_name
+      t.string :address
+      t.string :contact
+      t.string :aadhar_no
+      t.string :license
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_03_095041) do
+ActiveRecord::Schema.define(version: 2019_02_03_095729) do
+
+  create_table "users", force: :cascade do |t|
+    t.string "user_name"
+    t.string "address"
+    t.string "contact"
+    t.string "aadhar_no"
+    t.string "license"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "vehicles", force: :cascade do |t|
     t.string "vehicle_type"
